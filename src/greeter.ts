fa6f964e02e4f7a52225ea34f73d41a2643b9fc5
mochiflappe <mochiflappe@gmail.com>
2019-01-08 19:06:37 +0900

    tsをコンパイルできる所まで

---
+++ src/greeter.ts
@@ -0,0 +1,7 @@
+// aa
+export default class Greeter {
+    constructor(public greeting: string) { }
+    greet() {
+        return `<h1>#{this.greeting}</h1>`;
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import Greeter from './greeter';
+
+const greeter = new Greeter("Hello, world!");
+document.body.innerHTML = greeter.greet();
+
